25e451a86e9dae542502fc8865759420410d97a2
kleber <kleberposligua@gmail.com>
2018-11-15 09:49:07 -0500

    Commit inicial

---
+++ AppVectore/AppVectore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVectore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Control de versiones");
+            Console.ReadKey();
+        }
+    }
+}
